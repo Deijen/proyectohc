227453dfa7aaabb3818a14fa3a31bfcaff8ea01b
Diego Jesus Posada martinez <djpmz24@gmail.com>
2020-01-24 01:02:35 -0500

    agregar blade

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+Las rutas son las url de entrada de nuestra aplicación
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//practica rutas.
+Route::get('/', ['as'=> 'home' ,function(){
+	//echo "<a href=" . route('contactos') . ">Contacto</a><br>";
+
+	//echo "<a href= 'contactos'> contactos</a><br>";
+
+	//echo "<a href= 'contactos'> contactos</a><br>";
+
+	return view('home');
+}]);
+
+////////////////////////////////////
+/*
+darle nombre a las url o rutas.  Puesto que se hace referencia al nombre de la ruta y no a la url
+*/
+Route::get('contactame', ['as' => 'contactosNombre' , function(){
+	return 'hola desde contactos';
+
+}]);
+
+//ejemplo con parametro. El parametro es obligatorio en esta forma. Al agregar ? deja de ser obligatorio.
+
+route::get('saludos/{nombre?}', ['as' => 'saludoNombre' , function($nombre ="invitado"){
+	return view('saludo', compact('nombre')); //la funcion compact devuelve un array con la llave  nombre  y el valor nombre, siempre y cuando la variable "nombre" exista.
+}])->where('nombre', "[A-Za-z]*");
+
